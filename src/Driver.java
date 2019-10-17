687d7812e0ab8f2088eb6025f35db724e8c68ddd
Mike Litman <awesomefat@gmail.com>
2019-10-17 11:31:15 -0500

    small fix

--- src/Driver.java
+++ src/Driver.java
@@ -4,15 +4,15 @@ public class Driver
 	public static void main(String[] args) 
 	{
 		Investment smithYTY = new Investment();
-		smithYTY.addContribution(new Contribution(1300, 1));
+		smithYTY.addContribution(new Contribution(1200, 1));
 		int month = 13;
-		for(int i = 0; i < 30; i++)
+		for(int i = 0; i < 12; i++)
 		{
 			smithYTY.debitAccount(100, month);
 		    month = month + 12;
 		}
 		
-		int totalInvestmentTime = 360;
+		int totalInvestmentTime = 156;
 		System.out.println(smithYTY.getCurrentValue(totalInvestmentTime));
 	}
 }
